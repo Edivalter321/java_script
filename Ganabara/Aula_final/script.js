d8db42246a2999b9e8cbf4906c1bd2d2526f4d82
edivalter <junioredivalter25@gmail.com>
2024-02-26 09:51:48 -0300

    objetos

---
+++ Ganabara/Aula_final/script.js
@@ -0,0 +1,5 @@
+let amigo = {nome:'Antonio', sexo: 'M', idade:'28', endereco:'rua a, casa b', cidade:'fortaleza'}
+
+console.log(`${amigo.nome} tem ${amigo.idade} e mora em ${amigo.cidade}`)
+
+// Objetos em JS
