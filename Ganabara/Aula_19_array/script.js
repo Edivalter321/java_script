0033ba5f40c5b11d7f497e17c96def26134f9bbd
edivalter <junioredivalter25@gmail.com>
2024-02-26 09:29:36 -0300

    exercicios

---
+++ Ganabara/Aula_19_array/script.js
@@ -0,0 +1,37 @@
+let num = [5, 8, 4, 7, 9];
+num.push(1)
+num.sort()
+
+// num[3] = 6; -- adicionar um novo valor na vaga 3
+/*num.sort()
+num.push(80)
+num[4] = 50
+console.log(` ${num} O vetor possue: ${num.length} posições`)
+console.log(`O ultimo número do vetor é ${num[4]}`)
+*/
+
+// num.push(7) -- adicionar um novo elemento ao array
+// num.length -- comprimento do array/ vetor
+// num.sort() -- coloca em ordem crescente
+
+/*console.log(num[0])
+console.log(num[1])
+console.log(num[2])
+console.log(num[3])
+console.log(num[4])
+*/
+
+/*for (let pos = 0; pos < num.length; pos++) {
+  console.log(`A posicação ${pos} tem o valor: ${num[pos]} `)
+  
+}
+*/
+// Usando o for in
+/*for (let pos in num) {
+  console.log(`A posicação ${pos} tem o valor: ${num[pos]} `)
+}
+*/
+// buscando valores
+/*num.indexOf(4)
+num.indexOf(3) -1 (valor não encontrado)
+*/
---
+++ Ganabara/Aula_20-funcoes/script.js
@@ -0,0 +1,14 @@
+function parimp(n) {
+  // n = parâmetro
+
+  if (n % 2 == 0) {
+    // ação
+    return "par";
+  } else {
+    return "ímpar";
+  }
+}
+let res = parimp(11); // parimp = chamada
+// console.log(parim(11))
+console.log(res); // usar assim
+
